54ea5eef269d83b383db29474d92efa0bb7ced0e
saikrishna kovuru <121188181+saikrishnakovuru@users.noreply.github.com>
2023-11-11 23:10:30 +0000

    literalTypes using type

--- literals/LiteralTypes.ts
+++ literals/LiteralTypes.ts
@@ -1,3 +1,5 @@
+//================================================================================================
+// LiteralTypes using 'enum'.
 enum Evaluation {
   AsNumber,
   AsString,
@@ -14,4 +16,26 @@ const combineValues = function (
   return +value1 + +value2;
 };
 
-console.log(combineValues(10, 20, Evaluation.AsString));
+// console.log(combineValues(10,20,Evaluation.AsString));
+
+//================================================================================================
+// LiteralTypes using 'type'.
+type Signal = "RED" | "GREEN" | "ORANGE";
+
+const actOnSignal = function (signal: Signal): void {
+  switch (signal) {
+    case "RED":
+      console.log("Stop the vehicle");
+      break;
+    case "GREEN":
+      console.log("Drive the vehicle");
+      break;
+    case "ORANGE":
+      console.log("Start the vehicle");
+      break;
+    default:
+      console.log("none of the case matches");
+  }
+};
+
+actOnSignal("RED");
