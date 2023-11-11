8d476fefe7d7f0ea77ab9fff61ca6714bfdd1e3b
saikrishna kovuru <121188181+saikrishnakovuru@users.noreply.github.com>
2023-11-11 19:57:15 +0000

    Union

--- union/union.ts
+++ union/union.ts
@@ -2,8 +2,14 @@
 
 //in this example lets use a common function to compine numbers and string
 
-const combine = function (value1: number, value2: number):number {
-  return value1 + value2;
+const combine = function (
+  value1: number | string,
+  value2: number | string
+): number | string {
+  if (typeof value1 === "number" && typeof value2 === "number")
+    return value1 + value2;
+  return value1.toString() + value2.toString();
 };
 
-console.log(combine(20,20));
+console.log(combine(20, 20));
+console.log(combine("panda", " babai"));
