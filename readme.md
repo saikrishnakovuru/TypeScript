ee851f652ea53f829d5f8cc3023ca81bd62ac785
saikrishna kovuru <121188181+saikrishnakovuru@users.noreply.github.com>
2023-11-11 14:38:32 -0500

    readme Update

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 commad to run individual ts file is `npx ts-node union/union.ts`
+
+Another way to run the ts file is to simply click play button on the roght cornor.
---
+++ union/union.ts
@@ -0,0 +1,9 @@
+// a type that represents a value that can be of more than one type is an union
+
+//in this example lets use a common function to compine numbers and string
+
+const combine = function (value1: number, value2: number):number {
+  return value1 + value2;
+};
+
+console.log(combine(20,20));``
