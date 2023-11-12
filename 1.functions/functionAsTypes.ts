606f10ad0f7268cb9ffb86da6fa708409f231ca5
saikrishna kovuru <121188181+saikrishnakovuru@users.noreply.github.com>
2023-11-12 15:47:49 +0000

    function as types

---
+++ 1.functions/functionAsTypes.ts
@@ -0,0 +1,15 @@
+let operations: (num1: number, num2: number) => number;
+
+const addi = (operations = (num1, num2) => num1 + num2);
+const subs = (operations = (num1, num2) => num1 - num2);
+
+console.log(addi(10, 20));
+console.log(subs(10, 20));
+
+type Operations = (num1: number, num2: number) => number;
+
+const addition: Operations = (num1, num2) => num1 + num2;
+const substraction: Operations = (num1, num2) => num1 - num2;
+
+console.log(addition(10, 20));
+console.log(substraction(10, 20));
