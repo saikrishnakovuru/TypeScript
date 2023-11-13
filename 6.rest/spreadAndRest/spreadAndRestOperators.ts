a0f26e02da88307f536df3529b899cc0c6d85fbc
saikrishna kovuru <121188181+saikrishnakovuru@users.noreply.github.com>
2023-11-13 19:00:20 +0000

    notes updated

--- 6.rest/spreadAndRest/spreadAndRestOperators.ts
+++ 6.rest/spreadAndRest/spreadAndRestOperators.ts
@@ -29,7 +29,6 @@ const orderPasta = (...ing: string[]): void => {
 orderPasta(...restaurent.ingrediants);
 // in the above step we are passing arguments to orderPasta with `Spread operator`.
 //understand what spread operator does like, it spreads the values one by one, so the function should also be able to take the values one by one.
-
-// `Rest operator` takes teh values one by one.
+// `Rest operator` takes teh values one by one so we used rest operator in orderPasta to accept arguments.
 
 //=========================================================================================
