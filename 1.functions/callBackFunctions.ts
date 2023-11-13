908e34f817596304de6ab73e8c11dddfc372b435
saikrishna kovuru <121188181+saikrishnakovuru@users.noreply.github.com>
2023-11-13 05:48:16 +0000

    call backs

--- 1.functions/callBackFunctions.ts
+++ 1.functions/callBackFunctions.ts
@@ -1,15 +1,34 @@
-function delayMessage(
-  message: string,
-  delay: number,
-  callback: (msg: string) => void
+// function delayMessage(
+//   message: string,
+//   delay: number,
+//   callback: (msg: string) => string
+// ) {
+//   console.log(message);
+//   setTimeout(() => {
+//     console.log(callback(message));
+//   }, delay);
+// }
+
+// function finalCallback(message: string) {
+//   return "Callback completed for message: " + message;
+// }
+
+// const message: string = "Hello, World!";
+// delayMessage(message, 400, finalCallback);
+
+//================================================================================================
+
+const callingCallBackFunction = function (
+  callBack: (message: string) => string
 ) {
-  setTimeout(() => {
-    console.log(message);
-    callback(message);
-  }, delay);
-}
-function finalCallback(message: string) {
-  console.log("Callback completed for message: " + message);
-}
-const message: string = "Hello, World!";
-delayMessage(message, 400, finalCallback);
+  console.log(callBack("ra rei"));
+  console.log("I'm in higher order function");
+};
+
+const callBackFunction = function (message: string): string {
+  return "I'm in callBack function"+ message;
+};
+
+// Both the below statements are same
+callingCallBackFunction(callBackFunction);
+callingCallBackFunction((message)=>"I'm in callBack function"+ message);
